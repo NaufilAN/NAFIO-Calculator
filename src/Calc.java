4b052c47341399fa3fca737abda909810f732591
naufil_ansari_05 <naufilansari05@gmail.com>
2025-01-17 00:26:40 -0500

    Added label that tracks and displays numbers the user has entered

--- src/Calc.java
+++ src/Calc.java
@@ -11,33 +11,47 @@ public class Calc {
     calc.setResizable(false); //Ensures users don't break the formatting by resizing the window
     calc.setLayout(null);
     
-    //Button initialization from 0-9
-    addButton(calc, "0", 30, 500, 250, 50);
-    addButton(calc, "1", 30, 420, 50, 50);
-    addButton(calc, "2", 130, 420, 50, 50);
-    addButton(calc, "3", 230, 420, 50, 50);
-    addButton(calc, "4", 30, 340, 50, 50);
-    addButton(calc, "5", 130, 340, 50, 50);
-    addButton(calc, "6", 230, 340, 50, 50);
-    addButton(calc, "7", 30, 260, 50, 50);
-    addButton(calc, "8", 130, 260, 50, 50);
-    addButton(calc, "9", 230, 260, 50, 50);
+    //Label initialization
+    JLabel label = new JLabel();
+    calc.add(label);
+    label.setFont(new Font("Lucida Sans Typewriter", Font.PLAIN, 20));
+    label.setBounds(30, 50, 300, 50);
     
-    //Text window initialization
-    JTextField panel = new JTextField();
-    calc.add(panel);
-    panel.setBounds(30, 50, 300, 50);
+    //Button initialization from 0-9
+    addButton(calc, "0", 30, 500, 250, 50, label);
+    addButton(calc, "1", 30, 420, 50, 50, label);
+    addButton(calc, "2", 130, 420, 50, 50, label);
+    addButton(calc, "3", 230, 420, 50, 50, label);
+    addButton(calc, "4", 30, 340, 50, 50, label);
+    addButton(calc, "5", 130, 340, 50, 50, label);
+    addButton(calc, "6", 230, 340, 50, 50, label);
+    addButton(calc, "7", 30, 260, 50, 50, label);
+    addButton(calc, "8", 130, 260, 50, 50, label);
+    addButton(calc, "9", 230, 260, 50, 50, label);
+    
+//    //Text window initialization
+//    JTextField panel = new JTextField();
+//    calc.add(panel);
+//    panel.setBounds(30, 50, 300, 50);
     
     calc.setVisible(true);
     calc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Allows program to close when X'd out
   
   }
   
-  public static void addButton(JFrame calc, String label, int x, int y, int width, int height) {
+  public static void addButton(JFrame calc, String key, int x, int y, int width, int height, JLabel label) {
     
-    JButton button = new JButton(label);
+    JButton button = new JButton(key);
     calc.add(button);
     button.setBounds(x, y, width, height);
+    
+    button.addActionListener(e -> {
+      
+      String temp = label.getText();
+      temp += key;
+      label.setText(temp);
+      
+    });
     
   }
   
